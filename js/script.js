6d073611adcb2e89f141d87fef84562bbc0cd4fd
Rosamund Pearce <rosamundpearce@live.com>
2018-07-31 10:49:36 +0100

    Use data to dynamically generate markup

--- js/script.js
+++ js/script.js
@@ -10,7 +10,19 @@ $(function(){
     }];
 
     // pass in the data
-    $("#container").scrollStory({content: newspapers});
+    $("#container").scrollStory({
+        content: newspapers,
+        itembuild: function(ev, item){
+            // add title from data
+            item.el.append("<h2>"+item.data.organization+"</h2>");
+        },
+        itemfocus: function(ev, item){
+            // log the founding dates as you go past
+            console.log(item.data.organization + ", founded in " + item.data.founded + ", is now active!");
+        }
+    });
 
-    
+    console.log(newspapers);
+
+
 });
