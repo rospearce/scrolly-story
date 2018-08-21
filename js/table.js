01a2c083b89303294c9e56a33de890c7f4519cdb
Rosamund Pearce <rosamundpearce@live.com>
2018-08-21 15:19:25 +0100

    Updated the table data :construction:

--- js/table.js
+++ js/table.js
@@ -6,7 +6,10 @@ var getName = {
     "3.2C": "3.2C"
 }
 
+var newData;
+
 function tabulate(data, columns) {
+
     var table = d3.select(".table").append("table"),
         thead = table.append("thead"),
         tbody = table.append("tbody");
@@ -49,19 +52,19 @@ function tabulate(data, columns) {
 
 
 var species = [
-    { name: "Insects", "1.5C": "6%", "2C": "18%", "3.2C": "49%"},
-    { name: "Plants", "1.5C": "8%", "2C": "16%", "3.2C": "44%"},
-    { name: "Vertebrates", "1.5C": "4%", "2C": "8%", "3.2C": "26%"}
+    { name: "Insects", "1.5C": "6%", "2C": "18%", "3C": "49%"},
+    { name: "Plants", "1.5C": "8%", "2C": "16%", "3C": "44%"},
+    { name: "Vertebrates", "1.5C": "4%", "2C": "8%", "3C": "26%"}
 ];
 
 var rainfall = [
-    { name: "Extreme precipitation events (R99p)", "1.5C": "20%", "2C": "26%"},
-    { name: "Maximum 5-day rainfall total (RX5D)", "1.5C": "11%", "2C": "12%"},
-    { name: "Consecutive dry days (CDD)", "1.5C": "0%", "2C": "-5%"}
+    { name: "Extreme precipitation events (R99p)", "1.5C": "20%", "2C": "26%", "3C": ""},
+    { name: "Maximum 5-day rainfall total (RX5D)", "1.5C": "11%", "2C": "12%", "3C": ""},
+    { name: "Consecutive dry days (CDD)", "1.5C": "0%", "2C": "-5%", "3C": ""}
 ];
 
 // render the table
-var myTable = tabulate(species, ["name", "1.5C", "2C", "3.2C"]);
+var myTable = tabulate(species, ["name", "1.5C", "2C", "3C"]);
 
 // uppercase the column headers
 // myTable.selectAll("thead th")
@@ -73,7 +76,41 @@ var myTable = tabulate(species, ["name", "1.5C", "2C", "3.2C"]);
 myTable.selectAll('td:nth-child(1)')
 .style("font-weight", "bold");
 
-function updateTable () {};
+function updateTable (newData) {
+    
+    var rows = myTable.selectAll("tbody tr")
+    .data(newData, function (d) {return d.name;});
+    
+    rows.enter()
+    .append('tr')
+    .selectAll("td")
+    .data(function (d) {return [d.name, d["1.5C"], d["2C"], d["3C"] ];})
+    .enter()
+    .append("td")
+    .text(function(d) { return d; });
+    
+    rows.exit().remove();
+    
+    var cells = rows.selectAll('td')
+    .data(function (d) {return d.name, d["1.5C"], d["2C"], d["3C"];})
+    .text(function (d) {return d;});
+    
+    cells.enter()
+    .append("td")
+    .text(function(d) { return d; });
+    
+    cells.exit().remove();
+}
+
+setTimeout(function(){
+
+    newData = rainfall;
+
+    updateTable(newData);
+
+    console.log(newData);
+
+}, 3000);
 
 // sort by age
 // myTable.selectAll("tbody tr")
