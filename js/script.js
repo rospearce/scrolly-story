52a0940225a8fca22823fdac0b0b2631863b3bb0
Rosamund Pearce <rosamundpearce@live.com>
2018-09-05 16:37:39 +0100

    Add updating map text :sparkles:

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $(function(){
     var $droplinks = $('.droplinks')
     var $gradient = $('.story-gradient')
     var $map = $('.map')
+    var $mapText = $('#map-text')
     var windowHeight = $(window).height()
 
     var viewportHeight = window.innerHeight
@@ -25,21 +26,24 @@ $(function(){
         text: "Proportion of species losing >50% of their climatic range.",
         info: "Extra info 1 - Lorem ipsum dolor sit amet",
         background: "https://www.carbonbrief.org/wp-content/uploads/2018/04/Turtle-and-bleached-coral-on-Heron-Island-2.jpeg",
-        map: "australia"
+        map: "australia",
+        location: "Australia"
     },{
         name: "RAINFALL",
         icon: "fas fa-tint",
         info: "Extra info 2 - Lorem ipsum dolor sit amet",
         text: "Lorem ipsum dolor sit amet, eligendi scriptorem et nam.",
         background: "https://www.carbonbrief.org/wp-content/uploads/2018/01/C5T9H7-rainfall.jpg",
-        map: "latin-america"
+        map: "latin-america",
+        location: "Latin America"
     },{
         name: "GDP",
         icon: "fas fa-dollar-sign",
         info: "Extra info 3 - Lorem ipsum dolor sit amet",
         text: "Global per capita GDP in 2100 (relative to no additional warming).",
         background: "https://www.carbonbrief.org/wp-content/uploads/2015/07/Stock-wall-street-new-york-finance.jpg",
-        map: "global"
+        map: "global",
+        location: "Global"
     },{
         icon: "fas fa-info-circle",
         text: "Extra info here.<br><br>Data compiled by <a href='https://www.carbonbrief.org/author/robertmcsweeney'target='_blank'>Robert McSweeney.</a> Interactive by <a href='https://www.carbonbrief.org/author/rospearce' >Rosamund Pearce.</a> Licensed under <a href='https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode'>Creative Commons.</a>",
@@ -118,6 +122,7 @@ $(function(){
 
         if (item.index < 5 && item.index > 1) {
             $map.css( "background-image", "url('img/" + item.data.map + ".svg')");
+            $mapText.text(item.data.location);
         }
 
         // UPDATE TABLE
